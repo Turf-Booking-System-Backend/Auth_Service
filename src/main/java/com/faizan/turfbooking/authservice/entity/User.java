245cbdd8cfc4fcf14eab2ea42f44080037bb8f24
Faiz100-coder <faizanahmed5098k@gmail.com>
2026-01-17 13:21:52 +0530

    minor fixes

--- src/main/java/com/faizan/turfbooking/authservice/entity/User.java
+++ src/main/java/com/faizan/turfbooking/authservice/entity/User.java
@@ -14,7 +14,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
@@ -33,7 +32,7 @@ public class User {
 	private String email;
 	
 	@Column(nullable = false)
-		private String password;
+	private String password;
 	
 	
 	@Enumerated(EnumType.STRING)
